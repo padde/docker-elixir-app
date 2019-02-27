4d1a1c0b8f534cbbc5ae497a88c379bca521cc27
Patrick Oscity <patrick.oscity@cokron.com>
2019-02-27 17:44:44 +0100

    Configure phoenix to serve endpoints

--- config/prod.exs
+++ config/prod.exs
@@ -65,4 +65,6 @@ config :logger, level: :info
 # Note you can't rely on `System.get_env/1` when using releases.
 # See the releases documentation accordingly.
 
+config :phoenix, :serve_endpoints, true
+
 config :app, App.Repo, load_from_system_env: true
