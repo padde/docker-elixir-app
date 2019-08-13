73f1011add994e8407c6e8d0f9ffc8afc49c9eee
Patrick Oscity <patrick.oscity@cokron.com>
2019-08-13 11:11:53 +0200

    Update Hex packages

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule App.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:distillery, "~> 2.0.12"}
+      {:distillery, "~> 2.1.1"}
     ]
   end
 
